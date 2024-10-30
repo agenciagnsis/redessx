a28fa469f62de1b17719745a13a8217322f07a58
LEONARDO SILVA BARROS <agenciagnsis@gmail.com>
2024-10-29 23:45:50 -0300

    preloader.js

--- preloader.js
+++ preloader.js
@@ -1,7 +1,9 @@
+// Cria o overlay para o preloader
 const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     position: fixed;
@@ -9,41 +11,47 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(255, 255, 255, 0.7);
+    background: linear-gradient(135deg, #ff0000, #3a0a2b); /* Gradiente ajustado */
     z-index: 999;
 `;
 
+// Cria o contêiner para o Lottie
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
-    display: none;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    width: 150px;
+    height: 150px;
 `;
-lottieContainer.style.display = 'none';
 
+// Cria o texto de carregamento
+const loadingText = document.createElement('p');
+loadingText.innerText = 'Carregando...';
+loadingText.style.cssText = `
+    font-size: 1.5rem;
+    color: #ffffff;
+    margin-top: 20px;
+`;
+
+// Adiciona o contêiner Lottie e o texto ao overlay
+overlay.appendChild(lottieContainer);
+overlay.appendChild(loadingText);
+
+// Adiciona o overlay ao corpo da página
 document.body.appendChild(overlay);
-document.body.appendChild(lottieContainer);
 
-function hideOverlay() {
-    overlay.style.display = 'none';
-    lottieContainer.style.display = 'block';
-    // Substitua pela URL direta do seu arquivo JSON do Lottie.
-    lottie.loadAnimation({
-        container: lottieContainer,
-        renderer: 'svg', // ou 'canvas' se preferir
-        loop: true,
-        autoplay: true,
-        path: 'https://jvsc99.github.io/preloaderJSLottie/JVGIsylS3F-3.json',
-    });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    hideOverlay(); // Teste sem o setTimeout
+// Inicializa a animação Lottie
+lottie.loadAnimation({
+    container: lottieContainer,
+    renderer: 'svg', // ou 'canvas' se preferir
+    loop: true,
+    autoplay: true,
+    path: 'https://agenciagnsis.github.io/redessx/loading.json',
 });
 
+// Função para ocultar o overlay quando o carregamento estiver completo
+function hideOverlay() {
+    overlay.style.display = 'none';
+}
+
+// Ouvinte de evento para ocultar o overlay quando a página terminar de carregar
 window.addEventListener('load', hideOverlay);
